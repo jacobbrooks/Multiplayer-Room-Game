4d37d396f0f036d31d04dd05e88aef7fd6aeda58
domdew23 <ddewhurs@oswego.edu>
2018-04-29 15:54:25 -0400

    minor update

--- core/src/com/groupe/roomgame/networking/HoldElection.java
+++ core/src/com/groupe/roomgame/networking/HoldElection.java
@@ -1,5 +1,14 @@
 package com.groupe.roomgame.networking;
 
+import java.io.IOException;
+import java.net.MulticastSocket;
+
 public class HoldElection {
 
+	private final int PORT = 2703;
+	private final String HOST = "pi.cs.oswego.edu";
+	
+	public HoldElection() throws IOException {
+		MulticastSocket socket = new MulticastSocket();	
+	}
 }
